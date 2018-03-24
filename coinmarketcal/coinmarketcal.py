144b5ee3941d8f7143a1c95315cd020a076c47e1
Michael Füllbier <michael.fuellbier@posteo.de>
2018-03-24 13:24:58 +0100

    Update to version 1.0.0
    remove unneccessary comment

--- coinmarketcal/coinmarketcal.py
+++ coinmarketcal/coinmarketcal.py
@@ -1,20 +1,34 @@
+import configparser
 import json
 import requests as r
 
 
-def getCoins():
-    coins = r.get("https://coinmarketcal.com/api/coins")
+def getToken():
+    config = configparser.ConfigParser()
+    config.readfp(open('../config.txt'))
+    id = config.get('Coinmarketcal', 'id')
+    secret = config.get('Coinmarketcal', 'secret')
+    payload = {'grant_type': 'client_credentials', 'client_id': id, 'client_secret': secret}
+    response = r.post("https://api.coinmarketcal.com/oauth/v2/token", data=payload)
+    print(response.url)
+    return json.loads(response.text)
+
+
+def getCoins(token):
+    payload = {'access_token': token}
+    coins = r.get("https://api.coinmarketcal.com/v1/coins", params=payload)
     return json.loads(coins.text)
 
 
-def getCategories():
-    categories = r.get("https://coinmarketcal.com/api/categories")
+def getCategories(token):
+    payload = {'access_token': token}
+    categories = r.get("https://api.coinmarketcal.com/v1/categories", params=payload)
     return json.loads(categories.text)
 
 
-def getEvents(page=None, max=None, dateRangeStart=None, dateRangeEnd=None,
+def getEvents(token, page=None, max=None, dateRangeStart=None, dateRangeEnd=None,
               coins=None, categories=None, sortBy=None, showOnly=None):
-    params = {
+    payload = {
             "page": page,
             "max": max,
             "dateRangeStart": dateRangeStart,
@@ -23,8 +37,9 @@ def getEvents(page=None, max=None, dateRangeStart=None, dateRangeEnd=None,
             "categories": categories,
             "sortBy": sortBy,
             "showOnly": showOnly,
+            'access_token': token,
              }
 
-    url = "https://coinmarketcal.com/api/events"
-    events = r.get(url, params=params)
+    url = "https://api.coinmarketcal.com/v1/events"
+    events = r.get(url, params=payload)
     return json.loads(events.text)
